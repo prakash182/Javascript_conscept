9cfacfe8f5861880dc3d6dac1a67dec7ac530ed7
Prakash <109280432+prakash182@users.noreply.github.com>
2025-03-31 19:28:29 +0000

    learned about variables and its difference

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 12345
+
+let accountEmail = "prakash12@gmail.com"
+
+var password = "122221"
+
+city = "jodhpur"
+
+//accountId = 23423
+
+accountEmail = "prajapat@gmail.com"
+
+password = "232112"
+
+city = "bombay"
+
+
+/*
+mostlty we prefer const and let in javaScript, ignore var 
+
+let is block scope, var is function scope
+*/
+//console.log(accountId);
+console.table([accountId, accountEmail, password, city])
