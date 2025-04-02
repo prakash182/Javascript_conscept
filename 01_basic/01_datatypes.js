bbc767c9912994bc9fca425fbba34cfb79688a19
Prakash <109280432+prakash182@users.noreply.github.com>
2025-04-02 20:45:02 +0000

    done datatyeps

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,42 @@
+//Number
+const num = 5; //num
+let a = 4;
+
+console.log(typeof(num)) // when we run then it show number which is type of datatype
+//String
+
+let name = "prakash"
+console.log(name);
+
+console.log(typeof(name));  //string
+
+
+//Null
+
+let age = null; // standalone value
+
+console.log(age);
+
+console.log(typeof(age)); // it is showing type of a objectn but in undefined case it showing undefined , null is an object
+
+//Undefined
+
+let year;
+
+console.log(year);
+
+console.log(typeof(year)); //undefined
+
+//difference between null & undefined --> 
+/*
+in null variable we know that this variable contain null value which have not value assign, But in Undefined case their
+is not aware about variable this may be null or may not be null, it is surprise
+*/
+
+//BigInt --> use for long or large number
+
+//Boolean --->> use for true or false
+
+//Symbol  -->> unique 
+
+//Object   
