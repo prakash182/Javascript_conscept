18365f869699f6cbea85dd9dd2cf64efc03cc88d
Prakash <109280432+prakash182@users.noreply.github.com>
2025-04-07 21:54:24 +0000

    comparison done

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,16 @@
+console.log("2" > 1);
+console.log("2" == 2);
+console.log( "2"< 3);
+/* in javaScript automatically convert string into number thats why its outcome will be true in all three cases
+*/
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+
+// same thing happen also here that null treated as 0 that reason its output will be according to 0
+
+
+
+
+
