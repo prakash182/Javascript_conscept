a59f9062502abf046268f87f8ac04be35d4b2dfe
Prakash <109280432+prakash182@users.noreply.github.com>
2025-04-07 19:53:52 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let age = 23; // it is a number 
+
+console.log(typeof('' + age)); // it is conversion from number to string
+console.log(String(age));
+
+
+let name = "123"; // it is a string
+
+console.log(typeof(parseInt(name))); // it is conversion from string to number
+console.log(Number(name));
+
+
+let num = 123.7;
+console.log(typeof(parseFloat(num)));
+
+
+let score = "232sdx";
+
+let ValueInNumber = Number(score);
+
+console.log(typeof(ValueInNumber));
+
+console.log(ValueInNumber);
+
+//  true => 1,    false => 0
+// "" => false
+// "prakash" => true
+
+let pass = "prakash";
+
+console.log(typeof(Boolean(pass)));
+
+
