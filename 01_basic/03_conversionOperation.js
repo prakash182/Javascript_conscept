209e2e27164fd02ded040c5d5e69c339a9d5b819
Prakash <109280432+prakash182@users.noreply.github.com>
2025-04-07 21:42:53 +0000

    add on operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -31,3 +31,46 @@ let pass = "prakash";
 console.log(typeof(Boolean(pass)));
 
 
+// ****************************************************operations********************************************************************
+
+
+
+let value = 3;
+
+let nagetaiveNumber = - value;
+
+console.log(nagetaiveNumber);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+console.log(2 + "2");
+
+console.log("2" + 2);
+
+console.log("2" + "2");
+
+console.log(1 + 2 + "3");
+
+console.log("2" + 1 + 2);
+
+
+let num1, num2, num3 ; 
+num1 = num2 = num3 = 2+2 // it is wring way to declare, please ensure that try to code write in readability way
+
+let gameCounter = 100;
+gameCounter++; //postfix
+console.log(gameCounter); // 101
+
+++gameCounter; //prefix
+console.log(gameCounter); //101
+
+//postfix and prefix both are different => check difference on MDN
+
+
+
+
