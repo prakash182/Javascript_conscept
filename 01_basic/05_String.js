1b14f1c849467f1dbc8265c2e62f647cc05e411b
Prakash <109280432+prakash182@users.noreply.github.com>
2025-04-19 11:56:35 +0000

    string and ralated to it methods

---
+++ 01_basic/05_String.js
@@ -0,0 +1,72 @@
+const name = "prakash";
+const value = "50";
+const number = 34;
+
+// console.log(name + value + number); // this is a very worst method 
+
+// console.log(`my name is ${name} and this value is ${value} it is a ${number} number`); // it is best way to use it to concatenate strings 
+
+// const gameName = new String ('prakash-prajapat');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto___);
+
+// console.log(gameName.length); // it is give total length of the string
+
+// console.log(gameName.charAt(3)); // it is return the character of that particualar index 
+
+// console.log(gameName.indexOf('k')); // it is return the character of particular index position
+
+// console.log(gameName.toUpperCase()); // it is convert whole string in uppercase letters
+
+// console.log(gameName.toLowerCase()); // it is convert whole string in lower case letters
+
+// console.log(gameName.slice(2,7)); // it is return string from index 2 to index 6, it is not take the index 7
+
+// console.log(gameName.substring(3, 9));
+
+// console.log(gameName.slice(-7, 1));
+// console.log(gameName.substring(-7, 1));
+
+
+// console.log(gameName.trim(2, 6));
+
+// const cast = "    prajapati    ";
+// console.log(cast);
+
+// console.log(cast.trim()); // it's remove the whitespace from both side bit it's not affect the original object
+
+// const url  = "https//:prakashPrajapat//https//:prakash%20prajapat";
+
+ 
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('prakash'));
+
+
+const sentence = 'the fox is very hungry';
+console.log(sentence.split(' '));
+
+console.log(sentence.split(" ", 4));
+
+
+const myString = "Hello World. How are you doing?";
+const splits = myString.split(" ", 3);
+
+console.log(splits); // [ "Hello", "World.", "How" ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
